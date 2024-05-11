97e47a4b8a7bff0834463e092777dd1a080162e2
Tristan Johnston <Tristan.c.johnston@gmail.com>
2024-05-11 03:29:45 -0500

    added .getValue() to win conditions

--- script.js
+++ script.js
@@ -35,25 +35,25 @@ function Gameboard() {
 
         // Check rows
         for (let i=0; i<3; i++) {
-            if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            if (board[i][0].getValue() === board[i][1].getValue() && board[i][1].getValue() === board[i][2].getValue()) {
                 return 1
             }
-            if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            if (board[0][i].getValue() === board[1][i].getValue() && board[1][i].getValue() === board[2][i].getValue()) {
                 return 1
             }
         }
-        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        if (board[0][0].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][2].getValue()) {
             return 1
         }
-        if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        if (board[0][2].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][0].getValue()) {
             return 1
         }
-        else if (checkAvailable().length === 0) {
+        if (checkAvailable().length === 0) {
             return 2 // 2 = out of cells = draw
         }
-        else {
-            return 0 // continue game loop
-        }
+        
+        return 0 // continue game loop
+
     }
     return {board, pickCell, checkWin, checkAvailable,}
 };
